cfa0667f592e2441bbcb2ddfdde43d872b55efb4
Devin Spikowski <vegeta897@gmail.com>
2023-11-30 22:34:35 -0500

    ✨ Add parseInput function to template

--- template/index.ts
+++ template/index.ts
@@ -1,10 +1,19 @@
+const parseInput = (input: string) => {
+	return input
+		.trim()
+		.split('\n')
+		.map((v) => +v)
+}
+
 export const getPart1Answer = (input: string): string | number => {
+	const parsed = parseInput(input)
 	return ''
 }
 
 export const part1Examples = [[``, '']]
 
 export const getPart2Answer = (input: string): string | number => {
+	const parsed = parseInput(input)
 	return ''
 }
 
