b5aecca7a6309d0eb3523529a383194cf364fc1e
Devin Spikowski <vegeta897@gmail.com>
2023-11-30 22:40:34 -0500

    🧑‍💻 Log answers by default

--- template/index.test.ts
+++ template/index.test.ts
@@ -5,7 +5,7 @@ const inputText = await Bun.file(`${import.meta.dir}/input.txt`).text()
 
 test('solutions', () => {
 	part1Examples.forEach(([input, answer]) => expect(getPart1Answer(input)).toBe(answer))
-	// console.log('Part 1 answer:', getPart1Answer(inputText))
+	console.log('Part 1 answer:', getPart1Answer(inputText))
 	// part2Examples.forEach(([input, answer]) => expect(getPart2Answer(input)).toBe(answer))
-	// console.log('Part 2 answer:', getPart2Answer(inputText))
+	console.log('Part 2 answer:', getPart2Answer(inputText))
 })
