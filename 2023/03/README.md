42e05e9c637ed96d3d55651d175459113a9d1228
Devin Spikowski <vegeta897@gmail.com>
2023-12-03 00:43:25 -0500

    ✨ Add 2023 day 3 solutions

---
+++ 2023/03/README.md
@@ -0,0 +1,1 @@
+Wow, a grid puzzle so early! This was fun and satisfying because my first approaches worked great after squashing a few bugs.
---
+++ 2023/03/index.ts
@@ -0,0 +1,104 @@
+const parseInput = (input: string) => {
+	return input.trim().split('\n')
+}
+
+const isNum = (char: string) => !isNaN(+char)
+
+type GearNumber = { y: number; startX: number; endX: number; number: number }
+
+function getGearNumbers(input: string[]) {
+	const gearNumbers: GearNumber[] = []
+	for (let y = 0; y < input.length; y++) {
+		const line = input[y]
+		for (let x = 0; x < line.length; x++) {
+			const char = line[x]
+			if (isNum(char)) {
+				// Number
+				let numberString = char
+				const startX = x
+				let endX = x + 1
+				while (isNum(line[endX])) {
+					numberString += line[endX]
+					endX++
+				}
+				let isPartNumber = false
+				for (let ny = Math.max(0, y - 1); ny <= y + 1; ny++) {
+					if (isPartNumber) break
+					if (!input[ny]) continue
+					for (let nx = startX - 1; nx <= endX; nx++) {
+						const nChar = input[ny][nx]
+						if (nChar === undefined) continue
+						if (!isNum(nChar) && nChar !== '.') {
+							isPartNumber = true
+							break
+						}
+					}
+				}
+				if (isPartNumber) {
+					gearNumbers.push({
+						y,
+						startX,
+						endX: endX - 1,
+						number: parseInt(numberString),
+					})
+				}
+				x = endX
+			}
+		}
+	}
+	return gearNumbers
+}
+
+export const getPart1Answer: Answer = (input: string): string | number => {
+	const parsed = parseInput(input)
+	const gearNumbers = getGearNumbers(parsed)
+	return gearNumbers.map((g) => g.number).reduce((a, c) => a + c, 0)
+}
+
+export const part1Examples: Example[] = [
+	[
+		`467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`,
+		'4361',
+	],
+]
+
+export const getPart2Answer: Answer = (input: string): string | number => {
+	const parsed = parseInput(input)
+	const gearNumbers = getGearNumbers(parsed)
+	let sum = 0
+	for (let y = 0; y < parsed.length; y++) {
+		const line = parsed[y]
+		for (let x = 0; x < line.length; x++) {
+			if (line[x] !== '*') continue
+			const adjacentGears: Set<GearNumber> = new Set()
+			for (let ny = Math.max(0, y - 1); ny <= y + 1; ny++) {
+				if (!parsed[ny]) continue
+				for (let nx = x - 1; nx <= x + 1; nx++) {
+					if (ny === y && nx === x) continue // Skip * location
+					if (!isNum(parsed[ny][nx])) continue // Skip non-numbers
+					gearNumbers.forEach((nl) => {
+						if (nl.y === ny && nx >= nl.startX && nx <= nl.endX) {
+							adjacentGears.add(nl)
+						}
+					})
+				}
+			}
+			if (adjacentGears.size === 2) {
+				const numbers = [...adjacentGears.values()]
+				sum += numbers[0].number * numbers[1].number
+			}
+		}
+	}
+	return sum
+}
+
+export const part2Examples: Example[] = [[part1Examples[0][0], '467835']]
