de7ba6c6c21da33c89c0eef9f6abe0444701e93a
Devin Spikowski <vegeta897@gmail.com>
2023-12-04 03:36:14 -0500

    ✨ Add day create/run script, update readme

--- README.md
+++ README.md
@@ -4,11 +4,6 @@ This repo contains my solutions for Advent of Code 2023 and onward. It uses [Bun
 
 I didn't make a repo for 2021, but 2022 can be found [here](https://github.com/vegeta897/aoc2022)
 
-To solve a new day:
-
-1. Copy the contents of `/template` to `/YYYY/DD`
-2. Copy your puzzle input into input.txt
-3. Run `bun test --watch YYYY/DD`
-4. (Un)comment lines in the test file as needed.
+To solve a new day, run `bun run day {day}` or `bun run day {year} {day}`. If the year is omitted, the current year will be used. If the day folder doesn't already exist, the contents of `template` will be copied into it. Then the test runner will start in watch mode. Copy your puzzle input into `input.txt`, and (un)comment lines in `index.test.ts` as needed.
 
 _Repo structure inspired by [itswil's template](https://github.com/itswil/advent-of-code/)_
---
+++ scripts/day.ts
@@ -0,0 +1,44 @@
+// Create a new year/day folder if it doesn't already exist and start a test watcher
+
+import fs from 'node:fs'
+import path from 'node:path'
+
+function exitWithError(error: string) {
+	console.error(error)
+	process.exit(1)
+}
+
+const [dayOrYearArg, dayArg] = process.argv.slice(2)
+if (!dayOrYearArg) exitWithError('Missing day number')
+
+let year: number
+let day: number
+
+if (!dayArg) {
+	year = new Date().getFullYear()
+	day = parseInt(dayOrYearArg)
+} else {
+	year = parseInt(dayOrYearArg)
+	day = parseInt(dayArg)
+}
+if (isNaN(day) || day < 1 || day > 25) exitWithError('Invalid day number')
+if (isNaN(year) || year < 2015) exitWithError('Invalid year number')
+
+const dayPadded = `${day}`.padStart(2, '0')
+const dayPath = path.join(`${year}`, dayPadded)
+
+try {
+	// Create folder if it doesn't exist yet and copy files
+	const createdDir = fs.mkdirSync(dayPath, { recursive: true })
+	if (createdDir) {
+		console.log('Created directory', dayPath)
+		const glob = new Bun.Glob('*')
+		for await (const fileName of glob.scan({ cwd: 'template' })) {
+			console.log('Copying', fileName, 'to', dayPath)
+			const file = Bun.file(path.join('template', fileName))
+			await Bun.write(path.join(dayPath, fileName), file)
+		}
+	}
+} catch (_) {}
+
+Bun.spawn(['bun', 'test', '--watch', dayPath])
