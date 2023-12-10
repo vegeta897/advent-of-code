a08782eb15475c5c55eb928d835668cb625f566b
Devin Spikowski <vegeta897@gmail.com>
2023-12-10 09:41:09 -0500

    🧑‍💻 Tweak test

--- template/index.test.ts
+++ template/index.test.ts
@@ -5,10 +5,10 @@ import { logAnswer } from '@scripts/log'
 const inputText = await Bun.file(`${import.meta.dir}/input.txt`).text()
 
 test('solutions', () => {
+	console.log('  🌟 Part 1 answer:', getAndLogAnswer(1))
 	part1Examples.forEach(([i, a]) => expect(`${getPart1Answer(i)}`).toBe(`${a}`))
-	// console.log('  🌟 Part 1 answer:', getAndLogAnswer(1))
+	console.log('🌟🌟 Part 2 answer:', getAndLogAnswer(2))
 	part2Examples.forEach(([i, a]) => expect(`${getPart2Answer(i)}`).toBe(`${a}`))
-	// console.log('🌟🌟 Part 2 answer:', getAndLogAnswer(2))
 	expect(1).toBe(1) // Ensures that console logs always run
 })
 
