f333ac5990bf02027b1bb5bb36a4b588d01b96a0
Devin Spikowski <vegeta897@gmail.com>
2023-12-18 05:20:06 -0500

    ✨ Add 2023 day 18 solutions

---
+++ 2023/18/README.md
@@ -0,0 +1,13 @@
+Pretty easy part 1, though it was kind of inelegant and slow with a bunch of crawling. I've since removed that code after doing part 2.
+
+Part 2 made crawling impossible, and I had a very hard time coming up with a strategy to handle it.
+
+It turns out there's something called the shoelace formula, and you can use it to calculate the area of any polygon given its vertex coordinates. That sure sounds easy!
+
+I eventually came up with a strategy that can be pictured as waterfalls pouring down the puzzle shape. Every horizontal line encountered alters the waterfalls, either adding or removing segments, and the calculated area for the previous falls is added to the total.
+
+A lot of iteration went into reaching this final approach. I was doing all kinds of complex intersection and segment splitting that ended up being unnecessary. Turns out that storing the segments as just a flat list of stop and stop numbers lets you handle the transformations with a really simple method.
+
+So I'm really satisfied to have come up with and implemented my own algorithm despite it taking a while. I'm sure I would have reached for the shoelace formula if I knew about it, but I'm glad I didn't in retrospect.
+
+Rank #4984
---
+++ 2023/18/index.ts
@@ -0,0 +1,135 @@
+const parseInput = (input: string, p2 = false) =>
+	input
+		.trim()
+		.split('\n')
+		.map((line) => {
+			const [dirString, metersString, hexString] = line.split(' ')
+			const hex = hexString.slice(2, 8)
+			return {
+				dir: p2
+					? +hex.at(-1)!
+					: letterDirToEnum[dirString as keyof typeof letterDirToEnum],
+				meters: p2 ? parseInt(hex.slice(0, 5), 16) : +metersString,
+			}
+		})
+
+enum DIR {
+	right = 0,
+	down = 1,
+	left = 2,
+	up = 3,
+}
+
+const letterDirToEnum = {
+	U: DIR.up,
+	D: DIR.down,
+	R: DIR.right,
+	L: DIR.left,
+}
+
+const neighborXYs = [
+	[1, 0],
+	[0, 1],
+	[-1, 0],
+	[0, -1],
+]
+
+export const part1Examples: Example[] = [
+	[
+		`R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)`,
+		'62',
+	],
+]
+
+type Range = [start: number, end: number]
+
+const getOverlapWidth = (a: Range, b: Range) =>
+	Math.max(0, Math.min(a[1], b[1]) - Math.max(a[0], b[0]) + 1)
+
+function getTotalArea(lines: ReturnType<typeof parseInput>) {
+	const hStopsByY: Map<number, number[]> = new Map()
+	let x = 0
+	let y = 0
+	let startY = Infinity
+	for (const line of lines) {
+		const [dx, dy] = neighborXYs[line.dir]
+		const endX = x + dx * line.meters
+		const endY = y + dy * line.meters
+		if (dx !== 0) {
+			const hLine: Range = endX > x ? [x, endX] : [endX, x]
+			const hStopsAtY = hStopsByY.get(y)
+			if (hStopsAtY) {
+				hStopsAtY!.push(...hLine)
+			} else {
+				hStopsByY.set(y, hLine)
+			}
+		}
+		x = endX
+		y = endY
+		if (y < startY) startY = y
+	}
+	let waterfallY = startY
+	const hLineYs = [...hStopsByY.keys()].sort((a, b) => a - b)
+	const waterStops: number[] = hStopsByY.get(hLineYs.shift()!)!
+	let totalArea = 0
+	let prevWaterRanges: Range[] = [[waterStops[0], waterStops[1]]]
+	for (const hLineY of hLineYs) {
+		const height = hLineY - waterfallY + 1
+		for (let w = 1; w < waterStops.length; w += 2) {
+			const start = waterStops[w - 1]
+			const stop = waterStops[w]
+			const width = stop - start + 1
+			const area = width * height
+			totalArea += area
+		}
+		const stops = hStopsByY.get(hLineY)!.sort((a, b) => a - b)
+		for (let s = 0; s < stops.length; s++) {
+			const stop = stops[s]
+			const foundWaterStopIndex = waterStops.indexOf(stop)
+			if (foundWaterStopIndex >= 0) waterStops.splice(foundWaterStopIndex, 1)
+			else waterStops.push(stop)
+		}
+		waterStops.sort((a, b) => a - b)
+		const newWaterRanges: Range[] = []
+		for (let w = 1; w < waterStops.length; w += 2) {
+			newWaterRanges.push([waterStops[w - 1], waterStops[w]])
+		}
+		let totalOverlap = 0
+		for (const newWaterRange of newWaterRanges) {
+			for (const prevWaterRange of prevWaterRanges) {
+				const a = newWaterRange[0] <= prevWaterRange[0] ? newWaterRange : prevWaterRange
+				const b = a === newWaterRange ? prevWaterRange : newWaterRange
+				totalOverlap += getOverlapWidth(a, b)
+			}
+		}
+		totalArea -= totalOverlap
+		prevWaterRanges = newWaterRanges
+		waterfallY = hLineY
+	}
+	return totalArea
+}
+
+export const getPart1Answer: Answer = (input: string): string | number => {
+	const lines = parseInput(input)
+	return getTotalArea(lines)
+}
+
+export const getPart2Answer: Answer = (input: string): string | number => {
+	const lines = parseInput(input, true)
+	return getTotalArea(lines)
+}
+
+export const part2Examples: Example[] = [[part1Examples[0][0], '952408144115']]
