064e4ef388148d5912c8cf6f06b5abc4e0579231
Devin Spikowski <vegeta897@gmail.com>
2024-12-08 00:46:22 -0500

    🔥 Remove unnecessary diff reducing

--- 2024/08/README.md
+++ 2024/08/README.md
@@ -1,1 +1,3 @@
 Okay, pretty easy! Very satisfied that my part 2 solution worked without a hitch. It didn't take much thought! I only stumbled in part 1 a bit from a typo.
+
+Update: I even made it more complicated than it needed to be! I figured I would have to find the smallest whole-number portion of each diff, but there's no call for that in the input data.
--- 2024/08/index.ts
+++ 2024/08/index.ts
@@ -65,20 +65,12 @@ export const getPart2Answer: Answer = (input, example = false) => {
 			for (let n2 = n1 + 1; n2 < nodes.length; n2++) {
 				const node2 = nodes[n2]
 				const diff = [node2[0] - node1[0], node2[1] - node1[1]]
-				let smallestDiff = [...diff] as XY
-				for (let d = Math.min(...diff); d > 0; d--) {
-					if (
-						diff[0] / d === Math.round(diff[0] / d) &&
-						diff[1] / d === Math.round(diff[1] / d)
-					)
-						smallestDiff = [diff[0] / d, diff[1] / d]
-				}
-				for (let dir of [-1, 1]) {
+				for (const dir of [-1, 1]) {
 					let diffMult = dir
 					while (true) {
 						const antiNode: XY = [
-							node1[0] - smallestDiff[0] * diffMult,
-							node1[1] - smallestDiff[1] * diffMult,
+							node1[0] - diff[0] * diffMult,
+							node1[1] - diff[1] * diffMult,
 						]
 						if (!inBounds(antiNode, width, height)) break
 						antiNodeGrids.add(toGrid(...antiNode))
