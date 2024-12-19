c33511dd06446e923abf441f0864cbff26b16285
Devin Spikowski <vegeta897@gmail.com>
2024-12-19 01:09:30 -0500

    ✨ Add 2024 day 19 solutions

---
+++ 2024/19/README.md
@@ -0,0 +1,3 @@
+Pretty easy recursive/cache problem. Got delayed in part 1 from a stupid cache design mistake that happened to work for the example input.
+
+I have to use 2 caches in part 2 and I'm not entirely sure why it doesn't just work with the `countCache`.
---
+++ 2024/19/index.ts
@@ -0,0 +1,97 @@
+const parseInput = (input: string) => {
+	const [towelsStr, designsStr] = input.trim().split('\n\n')
+	const towels = towelsStr.split(', ')
+	const designs = designsStr.split('\n')
+	return { towels, designs }
+}
+
+const possibleCache: Map<string, boolean> = new Map()
+
+const canDesign = (design: string, remaining: string, towels: string[]): boolean => {
+	const cached = possibleCache.get(remaining)
+	if (cached !== undefined) return cached
+	for (const towel of towels) {
+		if (towel.length > remaining.length) continue
+		if (towel === remaining) {
+			possibleCache.set(remaining, true)
+			return true
+		}
+		if (towel === remaining.substring(0, towel.length)) {
+			const nowRemaining = remaining.substring(towel.length)
+			const complete = canDesign(design, nowRemaining, towels)
+
+			if (complete) {
+				possibleCache.set(remaining, complete)
+				return complete
+			}
+		}
+	}
+	possibleCache.set(remaining, false)
+	return false
+}
+
+export const getPart1Answer: Answer = (input, example = false) => {
+	const { towels, designs } = parseInput(input)
+	possibleCache.clear()
+	let validDesigns = 0
+	for (const design of designs) {
+		const validDesign = canDesign(design, design, towels)
+		if (validDesign) validDesigns++
+	}
+	return validDesigns
+}
+
+export const part1Examples: Example[] = [
+	[
+		`r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb`,
+		'6',
+	],
+]
+
+const countCache: Map<string, number> = new Map()
+
+const getDesignCount = (design: string, remaining: string, towels: string[]): number => {
+	const cachedPossible = possibleCache.get(remaining)
+	if (cachedPossible === false) return 0
+	const cachedCount = countCache.get(remaining)
+	if (cachedCount) return cachedCount
+	let count = 0
+	for (const towel of towels) {
+		if (towel.length > remaining.length) continue
+		if (towel === remaining) {
+			count++
+			continue
+		}
+		if (towel === remaining.substring(0, towel.length)) {
+			const newRemaining = remaining.substring(towel.length)
+			const newWays = getDesignCount(design, newRemaining, towels)
+			count += newWays
+		}
+	}
+	possibleCache.set(remaining, count > 0)
+	countCache.set(remaining, count)
+	return count
+}
+
+export const getPart2Answer: Answer = (input, example = false) => {
+	const { towels, designs } = parseInput(input)
+	possibleCache.clear()
+	countCache.clear()
+	let totalCount = 0
+	for (const design of designs) {
+		const count = getDesignCount(design, design, towels)
+		totalCount += count
+	}
+	return totalCount
+}
+
+export const part2Examples: Example[] = [[part1Examples[0][0], '16']]
